04a16dd59401510be92e4aabed1f155109ec1ded
Mohammad Reza <mthcom@live.com>
2019-05-21 22:10:03 +0430

    minor bugfix
    Co-author: H@di<hadi@hadisafari.ir>

--- src/main/java/Model/Student.java
+++ src/main/java/Model/Student.java
@@ -2,6 +2,7 @@ package main.java.Model;
 
 public class Student extends Person {
     private StudentID sid;
+    private MajorYear majorYear;
 
     public StudentID getSid() {
         return sid;
@@ -11,8 +12,17 @@ public class Student extends Person {
         this.sid = sid;
     }
 
-    public Student(PersonName name, NationalID nid, StudentID sid) {
+    public Student(PersonName name, NationalID nid, StudentID sid, MajorYear majorYear) {
         super(name, nid);
         this.sid = sid;
+        this.majorYear = majorYear;
+    }
+
+    public MajorYear getMajorYear() {
+        return majorYear;
+    }
+
+    public void setMajorYear(MajorYear majorYear) {
+        this.majorYear = majorYear;
     }
 }
