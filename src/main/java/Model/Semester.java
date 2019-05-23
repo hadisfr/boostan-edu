484f1ce808facd8c640493e322946eed30849f2b
h@di <hadi@hadisafari.ir>
2019-05-23 21:43:46 +0430

    add Semester

---
+++ src/main/java/Model/Semester.java
@@ -0,0 +1,17 @@
+package main.java.Model;
+
+public class Semester {
+    enum Term {Fall, Spring, Summer}
+    private int year;
+    private Term term;
+
+    public Semester(int year, Term term) {
+        this.year = year;
+        this.term = term;
+    }
+
+    @Override
+    public String toString() {
+        return year + term.toString();
+    }
+}
