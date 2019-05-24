c2e96d1c9a28cc0bba20c40f0a8aa2113c81daa3
Mohammad Reza <mthcom@live.com>
2019-05-24 19:40:57 +0430

    complete set grade
    Co-authored-by: H@di <hadi@hadisafari.ir>

--- src/main/java/Model/CourseEnrollment.java
+++ src/main/java/Model/CourseEnrollment.java
@@ -3,9 +3,33 @@ package main.java.Model;
 public class CourseEnrollment {
     private CourseEnrollmentState state;
     private Grade grade;
+    private Grade passGrade;
+    private CourseOffering courseOffering;
 
-    public CourseEnrollment(Grade grade) {
+    CourseEnrollment(CourseOffering courseOffering, Grade passGrade) {
+        if (passGrade.getType() != courseOffering.getGradeType())
+            throw new IllegalArgumentException();
         this.state = new CourseInProgress();
+        this.passGrade = passGrade;
+        this.courseOffering = courseOffering;
+    }
+
+    CourseEnrollment(Grade grade, CourseOffering courseOffering, Grade passGrade) {
+        this(courseOffering, passGrade);
+        setGrade(grade);
+    }
+
+    public void setGrade(Grade grade) {
+        if (grade.getType() != this.grade.getType())
+            throw new IllegalArgumentException();
         this.grade = grade;
+    }
+
+    Grade getGrade() {
+        return grade;
+    }
+
+    public CourseOffering getCourseOffering() {
+        return courseOffering;
     }
 }
