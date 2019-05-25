cb34e2a25d60f692388281d80eba94b68a55f6db
Mohammad Reza <mthcom@live.com>
2019-05-25 20:27:58 +0430

    for hadi
    Co-authored-by: H@di <hadi@hadisafari.ir>

---
+++ src/main/java/Model/CourseOfferingRepository.java
@@ -0,0 +1,31 @@
+package main.java.Model;
+
+import java.util.HashMap;
+
+public class CourseOfferingRepository {
+    private static HashMap<String, CourseOffering> database;
+
+    static  {
+        database = new HashMap<String, CourseOffering>();
+    }
+
+    public static void save(CourseOffering courseOffering) {
+        if (database.containsKey(courseOffering.toString())) {
+            CourseOffering currentCourseOffering = database.get(courseOffering.toString());
+            if (!courseOffering.equals(currentCourseOffering))
+                throw new IllegalArgumentException();
+        }
+        database.put(courseOffering.toString(), courseOffering);
+    }
+
+    public static CourseOffering get(String key) {
+        if (database.containsKey(key))
+            return database.get(key);
+        else
+            throw new IllegalArgumentException();
+    }
+
+//    public CourseOffering create(){
+//        CourseOffering newCourseOffering = new CourseOffering()
+//    }
+}
